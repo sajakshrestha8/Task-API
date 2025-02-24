880f4cf432c5d713e78238e757e7976e47cb4804
sajakshrestha8 <shresthasajak123@gmail.com>
2025-02-24 11:14:56 +0545

    Working on adding the task

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { Request, Response } from "express";
-import { sequelize, Task } from "./database";
 import { where } from "sequelize";
 
 const app = express();
 const Port = 8001;
 const fs = require('fs');
+const taskFile = 'task.json';
 
 //middleware
 app.use(express.json());
@@ -14,94 +14,51 @@ app.use(express.json());
 
 //route for getting all task 
 app.get('/tasks', async(req: Request, res: Response) => {
-    const allTask = await Task.findAll();
-    res.status(200).json(allTask);
+    const task = fs.readFileSync(taskFile);
+    res.json(JSON.parse(task));
 })
 
 //route to get task by Id
 app.get('/tasks/:id', async(req: Request, res: Response) => {
-    const taskId = req.params.id;
-    const task = await Task.findByPk(taskId);
-
-    if(!task){
-        res.status(404).send("Task not found");
-    }
-    res.status(200).json(task);
 })
 
 //route for adding the task
 app.post('/tasks', async(req: Request, res: Response) => {
-    const { task, status } = req.body;
+    const { id, task, status } = req.body;
+    
+    const data = await fs.readFile(taskFile, 'utf8');
 
-    const newTask = await Task.create({ task: task, status: status });
-
-    if(!task){
-        res.status(400).send("Task can't be empty")
+    if(data === undefined){
+        fs.writeFile(taskFile, '[]', (err: any) => console.log(err));
     }
-    res.status(200).json(newTask);
+    const fileTasks = data.trim() ? JSON.parse(data) : [] ;
+    const addedTask = await fs.appendFile(fileTasks, 
+       JSON.stringify( {
+            "id": id,
+            "task": task,
+            "status": status
+        }), (err: any) => {
+            if(err){
+                console.log(err)
+            }
+        })
+
+    res.json({
+        "message": "New task added to the file successfully",
+        "newTask": addedTask,
+    })
 })
 
 //route for updating the task
 app.put("/tasks/:id", async(req: Request, res: Response) => {
-    const taskId = req.params.id;
-    const { task, status } = req.body;
-
-    const existingTask = await Task.findByPk(taskId);
-    
-    if(!existingTask){
-        res.status(404).send("Task not found");
-    }
-
-    const updatedTask = await existingTask.update({ task, status });
-    res.status(200).json(updatedTask);
 })
 
 //route for toggling the status
 app.patch("/tasks/:id", async(req: Request, res: Response) => {
-    const taskId = req.params.id;
-
-    const existingTask = await Task.findByPk(taskId);
-
-    if(!existingTask){
-        res.status(404).send("Task not found");
-    }
-
-    let newStatus;
-    if(existingTask.status === "pending"){
-        newStatus = "completed";
-    }else if(existingTask.status === "completed"){    
-        newStatus = "pending";
-    }else{
-        res.status(400).send("No such status found");
-    }
-    
-    existingTask.status = newStatus;
-    await existingTask.save();
-
-    res.status(200).json({
-        "messege": "status toggled successfully", existingTask
-    });
 })
 
 //Delete the task by Id
 app.delete("/tasks/:id", async(req: Request, res: Response) => {
-    const taskId = req.params.id;
-
-    const task = await Task.findByPk(taskId);
-
-    const deletedTask = await Task.destroy({
-        where: {
-            id: taskId,
-        }
-    });
-    
-    if(deletedTask === 0){
-        res.status(404).send("No such task found");
-    }
-    res.status(200).json({
-        "messege": "Task has been deleted",
-        "task": task
-    });
 })
 
 app.listen(Port, () => {
